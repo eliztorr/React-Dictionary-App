83882333261a8cc9a7137257b44f8a8d77e0e9e3
Lizzie <57190069+eliztorr@users.noreply.github.com>
2024-04-15 17:37:23 +0200

    fixed issues, shows results

--- react-dictionary-app/src/Dictionary.js
+++ react-dictionary-app/src/Dictionary.js
@@ -31,6 +31,9 @@ export default function Dictionary(props) {
   function search() {
     let apiKey = "95302ab7f46ea49b23t9315bo4bc8de7";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    console.log("API URL:", apiUrl);
+
     axios.get(apiUrl).then(handleResponse);
   }
 
--- react-dictionary-app/src/Results.js
+++ react-dictionary-app/src/Results.js
@@ -4,20 +4,16 @@ import Meaning from "./Meaning";
 import "./Results.css";
 
 export default function Results(props) {
-  if (props.results) {
+
+  if (props.definition) {
     return (
       <div className="Results">
         <section>
-          <h2>{props.results.word}</h2>
-          {props.results.phonetics.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
+          <h1>{props.definition.word}</h1>
+          <Phonetic phonetic={props.definition.phonetic} />
         </section>
-        {props.results.meanings.map(function (meaning, index) {
+
+        {props.definition.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
               <Meaning meaning={meaning} />
